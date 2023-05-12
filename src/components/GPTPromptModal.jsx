6032a73e1ed3645d32d7aa563f744b3a925f5d39
Haroon Khan <hkhan6916@gmail.com>
2023-05-12 19:59:47 +0100

    remove image generation

--- src/components/GPTPromptModal.jsx
+++ src/components/GPTPromptModal.jsx
@@ -18,20 +18,10 @@ import useTypewriter from "../helpers/hooks/useTypeWriter";
 import { LinearGradient } from "expo-linear-gradient";
 import Constants from "expo-constants";
 
-const GPTPromptModal = ({
-  setShowModal,
-  active,
-  setText,
-  setPostImage,
-  generateImage,
-  isBio,
-  userData,
-}) => {
+const GPTPromptModal = ({ setShowModal, active, setText, isBio, userData }) => {
   const [error, setError] = useState("");
   const [generatingText, setGeneratingText] = useState(false);
-  const [generatingImage, setGeneratingImage] = useState(false);
   const [generatedText, setGeneratedText] = useState("");
-  const [generatedImageUrl, setGeneratedImageUrl] = useState("");
   const [gptPrompt, setGptPrompt] = useState("");
   const [prevTextToComplete, setPrevTextToComplete] = useState("");
   const [placeholder, setPlaceholder] = useState("");
@@ -40,9 +30,7 @@ const GPTPromptModal = ({
 
   const [disablePlaceholderTypewriter, setDisablePlaceholderTypewriter] =
     useState(false);
-  const placeholderTextArray = generateImage
-    ? []
-    : isBio
+  const placeholderTextArray = isBio
     ? [
         "I am web developer with 5 years experience...",
         "I am a digital marketer working at...",
@@ -80,156 +68,130 @@ const GPTPromptModal = ({
     });
     const openai = new OpenAIApi(configuration);
 
-    // NOT FEASABLE AT THE MOMENT, DALLE IS TOO EXPENSIVE TO USE NOW
-    if (generateImage) {
-      //   setGeneratingImage(true);
-      //   try {
-      //     const image = await openai.createImage({
-      //       prompt: `Generate an image of ${gptPrompt}`,
-      //       size: "512x512",
-      //     });
-      //     console.log({ image: image.data.data[0].url });
-      //     setGeneratedImageUrl(image.data.data[0].url);
-      //     setGeneratingImage(false);
-      //     // setGeneratingText(false);
-      //     // setGeneratedText(completionString);
-      //   } catch (error) {
-      //     setGeneratingImage(false);
-      //     if (error.response) {
-      //       console.log(error.response.status);
-      //       console.log(error.response.data);
-      //     } else {
-      //       console.log(error.message);
-      //     }
-      //   }
-    } else {
-      try {
-        setGeneratingText(true);
-        setGeneratedText("");
-        setPrevTextToComplete(gptPrompt);
+    try {
+      setGeneratingText(true);
+      setGeneratedText("");
+      setPrevTextToComplete(gptPrompt);
 
-        const jobTitle = userData?.jobTitle;
+      const jobTitle = userData?.jobTitle;
 
-        const currentJobs = userLatestJobHistories?.filter(
-          (job) => job.dateTo === "present"
-        );
+      const currentJobs = userLatestJobHistories?.filter(
+        (job) => job.dateTo === "present"
+      );
 
-        const pastJobs = userLatestJobHistories?.filter(
-          (job) => job.dateTo !== "present"
-        );
+      const pastJobs = userLatestJobHistories?.filter(
+        (job) => job.dateTo !== "present"
+      );
 
-        const currentEducation = userLatestEducationHistories?.filter(
-          (education) => education.dateTo === "present"
-        );
+      const currentEducation = userLatestEducationHistories?.filter(
+        (education) => education.dateTo === "present"
+      );
 
-        const pastEducation = userLatestEducationHistories?.filter(
-          (education) => education.dateTo !== "present"
-        );
+      const pastEducation = userLatestEducationHistories?.filter(
+        (education) => education.dateTo !== "present"
+      );
 
-        let autoGenerateBioPrompt = jobTitle ? `I work as a ${jobTitle}. ` : "";
-        if (userLatestJobHistories?.length) {
-          if (currentJobs?.length) {
-            autoGenerateBioPrompt +=
-              `I currently work ` +
-              currentJobs
-                .map((job) => `at ${job.companyName} as a ${job.roleName}`)
-                ?.join(", ") +
-              ".";
-            const currentJobsPrompt =
-              currentJobs
-                .map((job) => `at ${job.companyName} as a ${job.roleName}`)
-                ?.join(", ") + ".";
-            autoGenerateBioPrompt += currentJobsPrompt;
-          }
-
-          if (pastJobs?.length) {
-            autoGenerateBioPrompt += "I have previously worked ";
-            const pastJobsPrompt =
-              pastJobs
-                .map((job) => {
-                  return `at ${job.companyName} as a ${job.roleName} `;
-                })
-                ?.join("and ") + ".";
-            autoGenerateBioPrompt += pastJobsPrompt;
-          }
-        }
-        if (
-          // !userLatestJobHistories?.length &&
-          userLatestEducationHistories?.length
-        ) {
-          if (currentEducation?.length) {
-            autoGenerateBioPrompt +=
-              `I currently study ` +
-              currentEducation
-                .map(
-                  (education) =>
-                    `${education.educationName} at ${education.institutionName} `
-                )
-                ?.join(", ") +
-              ".";
-            const currentEducationPrompt =
-              currentEducation
-                .map(
-                  (education) =>
-                    `${education.educationName} at ${education.institutionName} `
-                )
-                ?.join(", ") + ".";
-            autoGenerateBioPrompt += currentEducationPrompt;
-          }
-
-          if (pastEducation?.length) {
-            autoGenerateBioPrompt += "I have previously studied ";
-            const pastEducationPrompt =
-              pastEducation
-                .map((job) => {
-                  return `at ${job.institutionName} as a ${job.educationName} `;
-                })
-                ?.join("and ") + ".";
-            autoGenerateBioPrompt += pastEducationPrompt;
-          }
+      let autoGenerateBioPrompt = jobTitle ? `I work as a ${jobTitle}. ` : "";
+      if (userLatestJobHistories?.length) {
+        if (currentJobs?.length) {
+          autoGenerateBioPrompt +=
+            `I currently work ` +
+            currentJobs
+              .map((job) => `at ${job.companyName} as a ${job.roleName}`)
+              ?.join(", ") +
+            ".";
+          const currentJobsPrompt =
+            currentJobs
+              .map((job) => `at ${job.companyName} as a ${job.roleName}`)
+              ?.join(", ") + ".";
+          autoGenerateBioPrompt += currentJobsPrompt;
         }
 
-        console.log({ autoGenerateBioPrompt });
-        // A work as a engineer. I currently work as a web dev at abc, a teacher at efg. I used to work as a tester at abc.
-        // return;
-        const completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [
-            {
-              role: "user",
-              content: `${
-                isBio
-                  ? "Create a bio for a user using the following"
-                  : "Create a post about the following"
-              } in no chronological order, without hashtags and without disclaimers: "${
-                useJobOrEducationHistoryForBio
-                  ? autoGenerateBioPrompt
-                  : gptPrompt
-              }". Limit the response to ${
-                useJobOrEducationHistoryForBio ? "1000" : "1000"
-              } characters.`,
-            },
-          ],
-          temperature: useJobOrEducationHistoryForBio ? 0.1 : 0.6,
-          max_tokens: 200,
-        });
-        const completionString =
-          completion.data.choices[0].message.content.trim();
-        setGeneratingText(false);
-        setGeneratedText(completionString);
-      } catch (error) {
-        setGeneratingText(false);
-        setError(
-          `There was a problem generating your ${
-            isBio ? "bio" : "post body"
-          }. Please try again later.`
-        );
-        if (error.response) {
-          console.log(error.response.status);
-          console.log(error.response.data);
-        } else {
-          console.log(error.message);
+        if (pastJobs?.length) {
+          autoGenerateBioPrompt += "I have previously worked ";
+          const pastJobsPrompt =
+            pastJobs
+              .map((job) => {
+                return `at ${job.companyName} as a ${job.roleName} `;
+              })
+              ?.join("and ") + ".";
+          autoGenerateBioPrompt += pastJobsPrompt;
         }
+      }
+      if (
+        // !userLatestJobHistories?.length &&
+        userLatestEducationHistories?.length
+      ) {
+        if (currentEducation?.length) {
+          autoGenerateBioPrompt +=
+            `I currently study ` +
+            currentEducation
+              .map(
+                (education) =>
+                  `${education.educationName} at ${education.institutionName} `
+              )
+              ?.join(", ") +
+            ".";
+          const currentEducationPrompt =
+            currentEducation
+              .map(
+                (education) =>
+                  `${education.educationName} at ${education.institutionName} `
+              )
+              ?.join(", ") + ".";
+          autoGenerateBioPrompt += currentEducationPrompt;
+        }
+
+        if (pastEducation?.length) {
+          autoGenerateBioPrompt += "I have previously studied ";
+          const pastEducationPrompt =
+            pastEducation
+              .map((job) => {
+                return `at ${job.institutionName} as a ${job.educationName} `;
+              })
+              ?.join("and ") + ".";
+          autoGenerateBioPrompt += pastEducationPrompt;
+        }
+      }
+
+      console.log({ autoGenerateBioPrompt });
+      // A work as a engineer. I currently work as a web dev at abc, a teacher at efg. I used to work as a tester at abc.
+      // return;
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `${
+              isBio
+                ? "Create a bio for a user using the following"
+                : "Create a post about the following"
+            } in no chronological order, without hashtags and without disclaimers: "${
+              useJobOrEducationHistoryForBio ? autoGenerateBioPrompt : gptPrompt
+            }". Limit the response to ${
+              useJobOrEducationHistoryForBio ? "1000" : "1000"
+            } characters.`,
+          },
+        ],
+        temperature: useJobOrEducationHistoryForBio ? 0.1 : 0.6,
+        max_tokens: 200,
+      });
+      const completionString =
+        completion.data.choices[0].message.content.trim();
+      setGeneratingText(false);
+      setGeneratedText(completionString);
+    } catch (error) {
+      setGeneratingText(false);
+      setError(
+        `There was a problem generating your ${
+          isBio ? "bio" : "post body"
+        }. Please try again later.`
+      );
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
       }
     }
   };
@@ -237,8 +199,7 @@ const GPTPromptModal = ({
   const disableWriteButton = !!(
     prevTextToComplete === gptPrompt ||
     generatingText ||
-    !gptPrompt ||
-    generatingImage
+    !gptPrompt
   );
 
   return (
@@ -303,11 +264,7 @@ const GPTPromptModal = ({
                 fontSize: 16,
               }}
             >
-              {generateImage
-                ? "Describe the image to generate for your post."
-                : isBio
-                ? "Talk about yourself."
-                : "What's on your mind?"}
+              {isBio ? "Talk about yourself." : "What's on your mind?"}
             </Text>
             <View
               style={{
@@ -328,9 +285,7 @@ const GPTPromptModal = ({
                   height: "100%",
                 }}
                 value={gptPrompt}
-                placeholder={
-                  generateImage ? "Keep it short and concise" : placeholder
-                }
+                placeholder={placeholder}
                 placeholderTextColor={themeStyle.colors.grayscale.high}
                 multiline
                 maxLength={150}
@@ -341,24 +296,22 @@ const GPTPromptModal = ({
                 }}
               />
             </View>
-            {!generateImage && (
-              <Text
-                style={{
-                  color: themeStyle.colors.grayscale.lowest,
-                  marginBottom: 20,
-                  paddingHorizontal: 5,
-                }}
-              >
-                {isBio
-                  ? `Start off your bio. Keep it short and concise, the rest will be generated${
-                      userLatestJobHistories?.length ||
-                      userLatestEducationHistories?.length
-                        ? ` or click "Auto Generate" to quickly generate your bio.`
-                        : "."
-                    }`
-                  : "Start off your post. Keep it short and concise."}
-              </Text>
-            )}
+            <Text
+              style={{
+                color: themeStyle.colors.grayscale.lowest,
+                marginBottom: 20,
+                paddingHorizontal: 5,
+              }}
+            >
+              {isBio
+                ? `Start off your bio. Keep it short and concise, the rest will be generated${
+                    userLatestJobHistories?.length ||
+                    userLatestEducationHistories?.length
+                      ? ` or click "Auto Generate" to quickly generate your bio.`
+                      : "."
+                  }`
+                : "Start off your post. Keep it short and concise."}
+            </Text>
           </>
           {gptPrompt?.length === 150 && (
             <Text
@@ -418,7 +371,7 @@ const GPTPromptModal = ({
                   }}
                 >
                   <Text style={{ color: themeStyle.colors.grayscale.lowest }}>
-                    {!generateImage ? "Start Writing" : "Generate Image"}
+                    Start Writing
                   </Text>
                 </View>
               </LinearGradient>
@@ -466,10 +419,7 @@ const GPTPromptModal = ({
               </TouchableOpacity>
             )}
           </View>
-          {generatedText ||
-          generatingText ||
-          generatedImageUrl ||
-          generatingImage ? (
+          {generatedText || generatingText ? (
             <View
               style={{
                 backgroundColor: themeStyle.colors.slateGray,
@@ -482,13 +432,9 @@ const GPTPromptModal = ({
                 style={{
                   flex: 1,
                 }}
-                contentContainerStyle={
-                  generateImage || generatingText || generatingImage
-                    ? { flex: 1 }
-                    : {}
-                }
+                contentContainerStyle={generatingText ? { flex: 1 } : {}}
               >
-                {generatingText || generatingImage ? (
+                {generatingText ? (
                   <View
                     style={{
                       flex: 1,
@@ -507,17 +453,8 @@ const GPTPromptModal = ({
                         fontWeight: "700",
                       }}
                     >
-                      {generateImage
-                        ? "Generating your image..."
-                        : `Writing your ${isBio ? "bio" : "post"}...`}
+                      Writing your {isBio ? "bio" : "post"}...
                     </Text>
-                  </View>
-                ) : generatedImageUrl ? (
-                  <View style={{ flex: 1, padding: 10 }}>
-                    <Image
-                      source={{ uri: generatedImageUrl }}
-                      style={{ width: "100%", height: "100%" }}
-                    />
                   </View>
                 ) : generatedText ? (
                   <Text
@@ -531,15 +468,11 @@ const GPTPromptModal = ({
                   </Text>
                 ) : null}
               </ScrollView>
-              {!!(generateImage || generatedText) && (
+              {!!generatedText && (
                 <View style={{ marginBottom: 10 }}>
                   <TouchableOpacity
                     onPress={() => {
-                      if (generateImage) {
-                        setPostImage(generatedImageUrl);
-                      } else {
-                        setText(generatedText);
-                      }
+                      setText(generatedText);
                       setShowModal(false);
                     }}
                     style={{
@@ -553,9 +486,7 @@ const GPTPromptModal = ({
                     }}
                   >
                     <Text style={{ color: themeStyle.colors.grayscale.lowest }}>
-                      {generateImage
-                        ? "Set As Post Image And Review"
-                        : isBio
+                      {isBio
                         ? "Set As Bio And Review"
                         : "Set As Post Body And Review"}
                     </Text>
